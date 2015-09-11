5fcaec5830f4d3be65295315c79e8acdec60ba24
flaviostutz <flaviostutz@github.com>
2015-09-11 05:32:30 +0000

    monot-vision workspace preparation

---
+++ src/monot-vision/tmp/CMakeFiles/monot-vision.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/monot-vision.dir/main.cpp.o"
+  "monot-vision.pdb"
+  "monot-vision"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/monot-vision.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
