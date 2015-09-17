aa1c4dd806e4180ca28b5ae5803e3167a0eab765
flaviostutz <flaviostutz@github.com>
2015-09-17 00:21:13 +0000

    First implementations

--- src/monot-vision/src/mqtt.c
+++ src/monot-vision/src/mqtt.c
@@ -1,7 +1,7 @@
 #include "stdio.h"
 #include "stdlib.h"
 #include "string.h"
-#include "MQTTAsync.h"
+#include <MQTTAsync.h>
 
 #define ADDRESS     "tcp://localhost:1883"
 #define CLIENTID    "ExampleClientPub"
